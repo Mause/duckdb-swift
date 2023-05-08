249ed3377190f7a8631046c9845c888c14bc0ab2
Elliana May <me@mause.me>
2023-05-08 10:41:41 +0800

    get diff

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@
 import PackageDescription
 
 let package = Package(
-
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+  ],
   name: "DuckDB",
   products: [
     .library(name: "DuckDB", targets: ["DuckDB"]),
